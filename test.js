905ca3a0dff8af69aa94a452d80f181982315280
Exeldinho <exeldinho@gmail.com>
2021-03-16 21:21:28 +0200

    Create

---
+++ test.js
@@ -0,0 +1,20 @@
+function bind (context, fn) {
+    return function(...args) {
+        fn.apply(context, args)
+    }
+}
+
+function logPerson () {
+        console.log(`Person: ${this.name}, ${this.age}, ${this.job}`)
+}
+
+
+const person1 = {name: 'Roman', age: 35, job: 'Frontend'}
+const person2 = {name: 'Tania', age: 25, job: 'Designer'}
+
+bind(person1, logPerson)()
+bind(person2, logPerson)()
+
+Document.getElementById('p').innerHTML = 'aaa'
+
+
