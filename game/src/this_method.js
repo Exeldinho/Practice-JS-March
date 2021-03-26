137986b6f9564a13ea20e902b3fbbc7c5b6b4f9a
Exeldinho <exeldinho@gmail.com>
2021-03-26 12:29:55 +0200

    practice with interacting components. Props

--- game/src/this_method.js
+++ game/src/this_method.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 
 class Button extends React.Component {
     scream() {
-        alert('All working Fine!');
+        alert("All work's Fine!");
     }
 
     render() {
